cf6da2e309e154084b04975ee26a23ffa62985e5
peel3r <ismc2222@gmail.com>
2017-01-24 11:28:48 +0000

    flashcards for programming revisions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in(@user)
-      flash[:notice] = "Welcome to your NightFlash!"
+      flash[:notice] = "Welcome!"
       redirect_to @user
     else
       render 'new'
